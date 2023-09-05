0c2fff588ffbc2fbbfa6efdf963b26663ff1a6e7
Ponchanon Datta Rone <pronecse@gmail.com>
2023-09-05 00:08:08 -0500

    update

--- UnoBold/Cloud Computing-Lectures/Project/website/custom-js/nav.js
+++
@@ -1,9 +0,0 @@
-$(".tab-parent").click(function(){
-	var tab_child = $(this).attr("child");
-	$(".tab-child").removeClass('active');
-	$(".tab-child-body").removeClass('active');
-	$(".tab-child-body").removeClass('in');
-  	$("#"+tab_child).addClass('active');
-  	$("#"+tab_child+"-body").addClass('active');
-  	$("#"+tab_child+"-body").addClass('in');
-});
